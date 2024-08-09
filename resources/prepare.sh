d3e2ce4ee1a016cb1a6039ff38b0e162d3db6dbe
Josh Burt <joshburt@Joshs-MBP.lan>
2024-08-08 18:57:13 -0700

    updates

--- resources/prepare.sh
+++ resources/prepare.sh
@@ -1,5 +1,5 @@
 source venv/bin/activate
-pip install -r ./requirements/requirements.docs.txt
+# pip install -r ./requirements/requirements.docs.txt
 pip install -r ./requirements/requirements.test.txt
 pip install -r ./requirements/requirements.txt
 pip install -r ./requirements/requirements.build.txt
