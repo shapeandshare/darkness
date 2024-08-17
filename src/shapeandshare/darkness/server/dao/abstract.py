0ce5745188934e222da2498c6321c273761ef63f
Josh Burt <joshburt@Joshs-MacBook-Pro.local>
2024-08-17 12:48:22 -0700

    cleanup

--- src/shapeandshare/darkness/server/dao/abstract.py
+++ src/shapeandshare/darkness/server/dao/abstract.py
@@ -64,11 +64,31 @@ class AbstractDao[T](BaseModel):
 
         return path / "metadata.json"
 
-    async def get(self, tokens: dict) -> WrappedData[T]:
-        logger.debug("[AbstractDao] getting document metadata from storage")
+    def _assert_metadata_exists(self, tokens: dict) -> Path:
         document_metadata_path: Path = self._document_path(tokens=tokens)
         if not document_metadata_path.exists():
             raise DaoDoesNotExistError("document metadata does not exist")
+        return document_metadata_path
+
+    def _assert_metadata_does_not_exists(self, tokens: dict) -> Path:
+        document_metadata_path: Path = self._document_path(tokens=tokens)
+        if document_metadata_path.exists():
+            raise DaoConflictError("document metadata already exists")
+        return document_metadata_path
+
+    def _assert_metadata_parent_exists(self, tokens: dict) -> Path:
+        document_metadata_path: Path = self._document_path(tokens=tokens)
+        if not document_metadata_path.parents[2].exists():
+            raise DaoDoesNotExistError("document container does not exist")
+        return document_metadata_path
+
+    @staticmethod
+    def _safe_create(document_metadata_path: Path):
+        if not document_metadata_path.parent.exists():
+            document_metadata_path.parent.mkdir(parents=True, exist_ok=True)
+
+    async def get(self, tokens: dict) -> WrappedData[T]:
+        document_metadata_path: Path = self._assert_metadata_exists(tokens=tokens)
         with open(file=document_metadata_path, mode="r", encoding="utf-8") as file:
             os.fsync(file)
             json_data: str = file.read()
@@ -79,14 +99,9 @@ class AbstractDao[T](BaseModel):
         """ """
 
     async def post_partial(self, tokens: dict, document: T, exclude: dict | None = None) -> WrappedData[T]:
-        entity_metadata_path: Path = self._document_path(tokens=tokens)
-        if entity_metadata_path.exists():
-            raise DaoConflictError("document metadata already exists")
-        if not entity_metadata_path.parents[2].exists():
-            raise DaoDoesNotExistError("document container does not exist")
-        if not entity_metadata_path.parent.exists():
-            logger.debug("[AbstractDao] document metadata folder creating ..")
-            entity_metadata_path.parent.mkdir(parents=True, exist_ok=True)
+        self._assert_metadata_does_not_exists(tokens=tokens)
+        document_metadata_path: Path = self._assert_metadata_parent_exists(tokens=tokens)
+        AbstractDao._safe_create(document_metadata_path=document_metadata_path)
 
         nonce: str = str(uuid.uuid4())
         wrapped_data: WrappedData[T] = WrappedData[T](data=document, nonce=nonce)
@@ -94,7 +109,7 @@ class AbstractDao[T](BaseModel):
         if exclude is not None:
             dump_params["exclude"] = exclude
         wrapped_data_raw: str = wrapped_data.model_dump_json(**dump_params)
-        with open(file=entity_metadata_path, mode="w", encoding="utf-8") as file:
+        with open(file=document_metadata_path, mode="w", encoding="utf-8") as file:
             file.write(wrapped_data_raw)
             os.fsync(file)
 
@@ -110,12 +125,7 @@ class AbstractDao[T](BaseModel):
         """ """
 
     async def put_partial(self, tokens: dict, wrapped_document: WrappedData[T], exclude: dict | None = None) -> WrappedData[T]:
-        document_metadata_path: Path = self._document_path(tokens=tokens)
-        if not document_metadata_path.parents[2].exists():
-            raise DaoDoesNotExistError("document container does not exist")
-        if not document_metadata_path.parent.exists():
-            logger.debug("[AbstractDAO] document metadata folder creating ..")
-            document_metadata_path.parent.mkdir(parents=True, exist_ok=True)
+        document_metadata_path: Path = self._assert_metadata_exists(tokens=tokens)
 
         # see if we have a pre-existing nonce to verify against
         try:
@@ -151,14 +161,7 @@ class AbstractDao[T](BaseModel):
         """ """
 
     async def patch_partial(self, tokens: dict, document: dict, exclude: dict | None = None) -> WrappedData[T]:
-        logger.debug("[AbstractDAO] patching document data in storage")
-        document_metadata_path: Path = self._document_path(tokens=tokens)
-        if not document_metadata_path.parents[2].exists():
-            raise DaoDoesNotExistError("document container does not exist")
-        if not document_metadata_path.parent.exists():
-            logger.debug("[WorldDAO] document metadata folder creating ..")
-            document_metadata_path.parent.mkdir(parents=True, exist_ok=True)
-
+        document_metadata_path: Path = self._assert_metadata_exists(tokens=tokens)
         previous_state: WrappedData[T] = await self.get(tokens=tokens)
 
         # if we made it this far we are safe to update
@@ -186,9 +189,6 @@ class AbstractDao[T](BaseModel):
         return stored_entity
 
     async def delete(self, tokens: dict) -> bool:
-        logger.debug("[AbstractDAO] deleting document data from storage")
-        document_metadata_path: Path = self._document_path(tokens=tokens)
-        if not document_metadata_path.exists():
-            raise DaoDoesNotExistError("document metadata does not exist")
+        document_metadata_path: Path = self._assert_metadata_exists(tokens=tokens)
         shutil.rmtree(document_metadata_path.parent)
         return True
