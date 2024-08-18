9301528f59132ee6036e1b35d90e706ee46b663c
Josh Burt <joshburt@users.noreply.github.com>
2024-08-17 18:14:28 -0700

    Jb 081724 (#17)

--- src/shapeandshare/darkness/server/factories/entity/abstract.py
+++ src/shapeandshare/darkness/server/factories/entity/abstract.py
@@ -45,12 +45,31 @@ class AbstractEntityFactory(BaseModel):
             await self.entitydao.post(tokens=tokens, document=new_entity)
             local_tile.data.ids.add(new_entity.id)
 
+            new_entity = Entity(id=str(uuid.uuid4()), entity_type=EntityType.MYCELIUM)
+            await self.entitydao.post(tokens=tokens, document=new_entity)
+            local_tile.data.ids.add(new_entity.id)
+
         elif local_tile.data.tile_type == TileType.OCEAN:
             new_entity: Entity = Entity(id=str(uuid.uuid4()), entity_type=EntityType.FISH)
             await self.entitydao.post(tokens=tokens, document=new_entity)
             local_tile.data.ids.add(new_entity.id)
 
         await self.tiledao.patch(tokens=tokens, document={"ids": local_tile.data.ids})
+
+    ### entity agent logic
+    async def entity_mycelium(self, wrapped_entity: WrappedData[Entity]):
+        pass
+
+    async def entity_grass(self, wrapped_entity: WrappedData[Entity]):
+        pass
+
+    async def entity_fish(self, wrapped_entity: WrappedData[Entity]):
+        pass
+
+    async def entity_tree(self, wrapped_entity: WrappedData[Entity]):
+        pass
+
+    ###
 
     async def grow_entities(self, tokens: dict):
         # get entities ids for the tile
@@ -67,6 +86,24 @@ class AbstractEntityFactory(BaseModel):
                     local_entity_id: str = await queue.get()
 
                     # TODO: Process entity
+                    wrapped_entity: WrappedData[Entity] = await self.entitydao.get(
+                        tokens={**tokens, "entity_id": local_entity_id}
+                    )
+                    wrapped_entity.data = Entity.model_validate(wrapped_entity.data)
+                    # print(wrapped_entity.model_dump())
+
+                    if wrapped_entity.data.entity_type == EntityType.MYCELIUM:
+                        await self.entity_mycelium(wrapped_entity=wrapped_entity)
+                    # elif wrapped_entity.data.entity_type == EntityType.MUSHROOM:
+                    #     pass
+                    elif wrapped_entity.data.entity_type == EntityType.GRASS:
+                        await self.entity_grass(wrapped_entity=wrapped_entity)
+                    elif wrapped_entity.data.entity_type == EntityType.FISH:
+                        await self.entity_fish(wrapped_entity=wrapped_entity)
+                    elif wrapped_entity.data.entity_type == EntityType.TREE:
+                        await self.entity_tree(wrapped_entity=wrapped_entity)
+                    # elif wrapped_entity.data.entity_type == EntityType.FRUIT:
+                    #     pass
 
                     queue.task_done()
 
