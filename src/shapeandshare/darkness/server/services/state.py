25dc449131033c3b9c83c2f193654f36ba9bb2df
Josh Burt <joshburt@Joshs-MacBook-Pro.local>
2024-08-18 11:41:06 -0700

    add entities

--- src/shapeandshare/darkness/server/services/state.py
+++ src/shapeandshare/darkness/server/services/state.py
@@ -2,16 +2,19 @@ import logging
 
 from pydantic import BaseModel
 
+from ...sdk.contracts.dtos.entities.entity import Entity
 from ...sdk.contracts.dtos.sdk.requests.chunk.create import ChunkCreateRequest
 from ...sdk.contracts.dtos.sdk.requests.chunk.delete import ChunkDeleteRequest
 from ...sdk.contracts.dtos.sdk.requests.chunk.get import ChunkGetRequest
 from ...sdk.contracts.dtos.sdk.requests.world.create import WorldCreateRequest
 from ...sdk.contracts.dtos.sdk.requests.world.delete import WorldDeleteRequest
 from ...sdk.contracts.dtos.sdk.requests.world.get import WorldGetRequest
+from ...sdk.contracts.dtos.sdk.wrapped_data import WrappedData
 from ...sdk.contracts.dtos.tiles.chunk import Chunk
 from ...sdk.contracts.dtos.tiles.tile import Tile
 from ...sdk.contracts.dtos.tiles.world import World
 from ..dao.chunk import ChunkDao
+from ..dao.entity import EntityDao
 from ..dao.tile import TileDao
 from ..dao.world import WorldDao
 from ..factories.chunk.flat import FlatChunkFactory
@@ -25,6 +28,7 @@ class StateService(BaseModel):
     worlddao: WorldDao
     chunkdao: ChunkDao
     tiledao: TileDao
+    entitydao: EntityDao
     world_factory: WorldFactory
     entity_factory: EntityFactory
     flatchunk_factory: FlatChunkFactory
@@ -41,9 +45,11 @@ class StateService(BaseModel):
     async def world_get(self, request: WorldGetRequest) -> World:
         # Build a complete World from Lite objects
         world: World = World.model_validate((await self.worlddao.get(tokens={"world_id": request.id})).data)
-        chunk_ids: set[str] = world.ids
+
         partial_world = world.model_dump(exclude={"ids"})
         world: World = World.model_validate(partial_world)
+
+        chunk_ids: set[str] = world.ids
         for chunk_id in chunk_ids:
             local_chunk: Chunk = await self.chunk_get(request=ChunkGetRequest(world_id=request.id, chunk_id=chunk_id))
             world.contents[chunk_id] = local_chunk
@@ -89,19 +95,47 @@ class StateService(BaseModel):
         chunk: Chunk = Chunk.model_validate(
             (await self.chunkdao.get(tokens={"world_id": request.world_id, "chunk_id": request.chunk_id})).data
         )
-        tile_ids: set[str] = chunk.ids
+
         chunk_partial = chunk.model_dump(exclude={"tile_ids"})
         chunk: Chunk = Chunk.model_validate(chunk_partial)
 
         # re-hydrate the tiles
+        tile_ids: set[str] = chunk.ids
         for tile_id in tile_ids:
-            tile: Tile = Tile.model_validate(
-                (
-                    await self.tiledao.get(
-                        tokens={"world_id": request.world_id, "chunk_id": chunk.id, "tile_id": tile_id}
-                    )
-                ).data
+            tile: Tile = await self.tiledao.get(
+                tokens={"world_id": request.world_id, "chunk_id": chunk.id, "tile_id": tile_id}
             )
+
+            # re-hydrate the entities
+            entity_ids: set[str] = tile.ids
+            for entity_id in entity_ids:
+                entity: Entity = await self.entity_get(
+                    tokens={
+                        "world_id": request.world_id,
+                        "chunk_id": chunk.id,
+                        "tile_id": tile_id,
+                        "entity_id": entity_id,
+                    }
+                )
+
+                # add finalized entity to tile
+                tile.contents[entity_id] = entity
+
+            # Add finalized tile to chunk
             chunk.contents[tile_id] = tile
 
         return chunk
+
+    ### Tile ##################################
+
+    async def tile_get(self, tokens: dict) -> Tile:
+        wrapped_tile: WrappedData[Tile] = await self.tiledao.get(tokens=tokens)
+        tile: Tile = Tile.model_validate(wrapped_tile.data)
+        return tile
+
+    ### Entity ##################################
+
+    async def entity_get(self, tokens: dict) -> Entity:
+        wrapped_entity: WrappedData[Entity] = await self.entity_dao.get(tokens=tokens)
+        entity: Entity = Entity.model_validate(wrapped_entity.data)
+        return entity
