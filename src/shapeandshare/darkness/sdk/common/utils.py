304af50b92c1d832e644df1fd0e8b502cf7b2789
Josh Burt <joshburt@Joshs-MacBook-Pro.local>
2024-08-23 19:25:24 -0700

    removals

--- src/shapeandshare/darkness/sdk/common/utils.py
+++ src/shapeandshare/darkness/sdk/common/utils.py
@@ -15,29 +15,6 @@ def address_type(address: Address) -> DaoDocumentType:
     raise Exception("Unknown address type")
 
 
-def document_path(address: Address, doc_type: DaoDocumentType) -> str:
-    path: str = "world/"
-
-    assert address.world_id is not None
-    path += address.world_id
-    if doc_type != DaoDocumentType.WORLD:
-
-        assert address.chunk_id is not None
-        path += "/chunk/" + address.chunk_id
-        if doc_type != DaoDocumentType.CHUNK:
-
-            assert address.tile_id is not None
-            path += "/tile/" + address.tile_id
-            if doc_type != DaoDocumentType.TILE:
-
-                assert address.entity_id is not None
-                path += "/entity/" + address.entity_id
-                if doc_type != DaoDocumentType.ENTITY:
-                    raise Exception("Unknown document type requested!")
-
-    return path
-
-
 def get_document_id_from_address(address: Address, doc_type: DaoDocumentType | None = None) -> str:
     if doc_type is None:
         doc_type = address_type(address)
--- src/shapeandshare/darkness/sdk/contracts/dtos/sdk/requests/document/patch.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-from ....tiles.address import Address
-
-
-class DocumentPatchRequest(BaseModel):
-    address: Address
-    document: dict
