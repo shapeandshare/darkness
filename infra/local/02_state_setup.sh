5724160bf19f5b448512f6b75717287c6aed3f50
Josh Burt <joshburt@users.noreply.github.com>
2024-09-15 11:44:00 -0700

    wrapped process control enhancement (#32)

--- infra/local/02_state_setup.sh
+++ infra/local/02_state_setup.sh
@@ -3,8 +3,10 @@ if [ -d infra/local/venv ]; then
 else
   echo "setting up python runtime environment ..."
   python3 -m venv infra/local/venv
-  source infra/local/venv/bin/activate
-  pip install --upgrade pip
-  pip install -r infra/local/requirements.server.txt
-  deactivate
 fi
+
+echo "ensuring python environment is up to date ..."
+source infra/local/venv/bin/activate
+pip install --upgrade pip
+pip install -r infra/local/requirements.server.txt
+deactivate
--- infra/local/shutdown.sh
+++ infra/local/shutdown.sh
@@ -1,4 +1,4 @@
 for session_id in $(screen -ls | grep -i darkness- | awk '{print $1}'); do
   echo "terminating $session_id"
-  screen -XS $session_id quit
+  screen -XS $session_id kill
 done
